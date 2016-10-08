5357f4198141c9f0af0dbf9e3cd79d0570e87c76
[GaryThuc Nguyen] <thucnguyen1295@gmail.com>
2016-10-07 18:27:12 -0700

    Finalize code for regression-script.R

--- code/regression-script.R
+++ code/regression-script.R
@@ -1,5 +1,5 @@
 
-eads in the Advertising.csv data set, and computes a
+Reads in the Advertising.csv data set, and computes a
 "regression" object-via lm() -as well as the summary of such regression object-
 via summary() . This script also produces the scatterplot with the regression line.
 The R objects from the regression analysis are saved in the file regression.RData.
@@ -7,29 +7,28 @@ In turn, the scatterplot is saved in both PNG and PDF formats.
 
 
 Advertising_data <- read.csv("Advertising.csv", header = TRUE)
+reg= lm(Advertising_data$Sales ~ Advertising_data$TV, data = Advertising_data)
+
+#Using save function 
+save(reg, file = 'data/regression.RData')
+summary(reg)
 
 
 # create the scatterplot-tv-sales.png
 
-png(file = "/Users/Thuc/Dropbox/UC_Berkley/FALL_2016/STAT_159/homework_2/stat159-fall2016-hw02/images/scatterplot-tv-sales.png")
+png(file = "images/scatterplot-tv-sales.png", height= 4.5, width = 7)
+
+plot(Advertising_data$TV, Advertising_data$Sales, pch = 20, col = "deeppink3", ylab = "Sales", xlab = "TV")
+title(main = 'Advertising Scatterplot of  Sales vs. TV')
+abline(lm.fit, lwd = 2)
+dev.off()
 
 
+# create the scatterplot-tv-sales.pdf
 
-plot()
+pdf(file = "images/scatterplot-tv-sales.pdf")
+plot(Advertising_data$TV, Advertising_data$Sales, pch = 20, col = "deeppink3", ylab = "Sales", xlab = "TV")
+title(main = 'Advertising Scatterplot: Sales vs. TV')
+abline(lm.fit, lwd = 2)
+dev.off()
 
-
-
-
-plot( dose, drugA, type=" b" ,
-      pch=15, lty=1, col=" red" , ylim=c( 0, 60) ,
-      main=" Scatter plot of " ,
-      xlab=" Drug Dosage" , ylab=" Drug Response" )
-lines( dose, drugB, type=" b" ,
-       pch=17, lty=2, col=" blue" )
-abline( h=c( 30) , lwd=1. 5, lty=2, col=" gray" )
-
-## Use save() function to produce 
-save()
-
-reg = lm(eda$Sales~eda$TV)
-save(reg, file = "/Users/MandyZhang/Desktop/stat_159/hw02/data/regression.RData")
