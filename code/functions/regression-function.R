90c6d8a523c37ed8cc2f9a17d31ddab60f2a64a7
[GaryThuc Nguyen] <thucnguyen1295@gmail.com>
2016-10-14 11:44:38 -0700

    regression-function.R with added comments on description, input, output

--- code/functions/regression-function.R
+++ code/functions/regression-function.R
@@ -3,6 +3,10 @@
 #output is the RSS. See the formula of eq. 3.16 (page 69).
 
 # Residual Sum of Squares
+# Function computes Residual Sum of Squares 
+# Expected in put is an 'lm object', denoted by x
+# Return output is RSS of the 'lm object'
+
 Residual_ss <- function(x) {
   RSS = sum((x$residuals)^2)
 }
@@ -15,11 +19,16 @@ Total_ss <- function(w){
   sum((y-mean(y))^2)
 }
 
+
+#Expect input is lm object, return output is R-squared value
+#Compute R-squared (p.70)
 ## R-squared value
 R_squared = function(w){
   1-Residual_ss(w)/Total_ss(w)
 }
 
+#Expect input is lm object, return output is F-statistics 
+#Compute f-statistics (p.75)
 ## F-statistics
 
 F_statistic = function(w){
@@ -29,6 +38,8 @@ F_statistic = function(w){
   (Total_ss(w) - Residual_ss(w))/p/(Residual_ss(w)/(n-p-1))
 }
 
+#Expect input is lm object, return output is RSE
+#Compute residual standard error (p.80)
 ## Residual Standard Error
 residual_std_error = function(w){
   p = w$rank - 1
